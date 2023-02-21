0e30076913e848f0022b5dec636d69c9346ce733
OleksandrBeley <beleyoleksandrrsschool@gmail.com>
2023-02-21 16:11:44 +0200

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let numTostr = String(n);
+    console.log(numTostr)
+    if (numTostr[0] === '-') {
+        numTostr = numTostr.slice(1);
+    }
+    return +numTostr.split('').reverse().join('');
 }
